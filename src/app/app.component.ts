213c3246be41754596fe5de4226b50a23e8097ae
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-23 18:56:21 -0300

    feat: criando funcao para filtrar os contatos por nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,52 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './componentes/container/container.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './componentes/separador/separador.component';
+import { ContatoComponent } from './componentes/contato/contato.component';
+import { Contato } from './componentes/contato/contato';
+import { FormsModule } from '@angular/forms';
+
+import agenda from './agenda.json'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Array<Contato> = agenda;
+
+  filtroPorTexto: string = '';
+
+  private removerAcentos(texto: string): string {
+    return texto.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+
+  filtrarContatoPorNome(): Contato[] {
+    if (!this.filtroPorTexto) {
+      return this.contatos;
+    }
+    return this.contatos.filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase());
+    })
+    
+  }
+
+  filtrarContatoPorLetraInicial(letra: string): Contato[] {
+    return this.filtrarContatoPorNome().filter(contato => {
+      return this.removerAcentos(contato.nome.toLowerCase()).startsWith(this.removerAcentos(letra).toLowerCase());
+    })
+  }
+
 }
