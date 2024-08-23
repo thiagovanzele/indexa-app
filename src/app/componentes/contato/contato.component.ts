eace321001ddff2483f8d9f5283c84018fecf856
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-23 18:55:38 -0300

    feat: contato componente adicionado

---
+++ src/app/componentes/contato/contato.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-contato',
+  standalone: true,
+  imports: [],
+  templateUrl: './contato.component.html',
+  styleUrl: './contato.component.css'
+})
+
+export class ContatoComponent {
+  @Input() nome: string = '';
+  @Input() telefone: string = '';
+
+}
